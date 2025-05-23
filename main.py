6f79a8294b2588c026dd327d573fac19d4f69e3a
Marcus Rådell <marcus@radell.net>
2025-05-23 14:00:21 +0200

    code along up to 43:56

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import requests
 from qdrant_client import QdrantClient
-from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import Distance, VectorParams, PointStruct
+
+dummy_data = [
+    "My name is Marcus",
+    "I like Helix",
+    "I like Rio",
+    "I love my kids",
+    "My name is Rådell",
+]
 
 client = QdrantClient(url="http://localhost:6333")
 
@@ -13,12 +21,18 @@ if not client.collection_exists(collection_name="demo"):
 
 
 def main():
-    response = requests.post(
-        "http://localhost:11434/api/embed",
-        json={"model": "mxbai-embed-large", "input": "Hello, world!"},
-    )
-    data = response.json()
-    print(data)
+    for i, text in enumerate(dummy_data):
+        response = requests.post(
+            "http://localhost:11434/api/embed",
+            json={"model": "mxbai-embed-large", "input": "Hello, world!"},
+        )
+        data = response.json()
+        embeddings = data["embeddings"][0]
+        client.upsert(
+            collection_name="demo",
+            wait=True,
+            points=[PointStruct(id=i, vector=embeddings, payload={"text": text})],
+        )
 
 
 if __name__ == "__main__":
